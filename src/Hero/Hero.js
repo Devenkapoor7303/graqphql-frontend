ba6fc164cb32df225b8807ac519aa3cbb9f8231c
deven kapoor <deven.kapoor1999@gmail.com>
2023-08-28 18:52:28 +0530

    first commit

---
+++ src/Hero/Hero.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Hero.css';
+
+function HeroSection() {
+  return (
+    <div className="hero-container">
+      <h1>Your Todo App</h1>
+      <p>Stay organized and get things done.</p>
+    </div>
+  );
+}
+
+export default HeroSection;
---
+++ src/Login/Login.js
@@ -0,0 +1,78 @@
+import React from "react";
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
+
+const Login = () => {
+    const [email, setemail] = useState("");
+    const [password, setpassword] = useState("");
+
+    const navigate = useNavigate();
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const result = await fetch("http://localhost:3000/api/graph-api", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                query: `
+                query{
+                    loginUser(userInput:{email:"${email}",password:"${password}"}){
+                      name
+                      email
+                    }
+                  }
+                `,
+            }),
+        });
+        const user_id = await result.json();
+        localStorage.setItem("user", JSON.stringify(user_id));
+        setemail("");
+        setpassword("");
+        navigate("/");
+    };
+
+    return (
+        <>
+            <main>
+                <form onSubmit={handleSubmit}>
+                    <h2>
+                        <strong>Welcome to TodoApp!</strong>
+                    </h2>
+                    <div className="user-box">
+                        <input
+                            type="email"
+                            autoComplete="off"
+                            name="username"
+                            onChange={(e) => {
+                                setemail(e.target.value);
+                            }}
+                            value={email}
+                            required
+                        />
+                        <label className="label-class">Email</label>
+                    </div>
+                    <div className="user-box">
+                        <input
+                            type="password"
+                            autoComplete="off"
+                            name="password"
+                            onChange={(e) => {
+                                setpassword(e.target.value);
+                            }}
+                            value={password}
+                            required
+                        />
+                        <label className="label-class">Password</label>
+                    </div>
+                    <button className="login-signup-button" type="submit">
+                        Log In
+                    </button>
+                </form>
+            </main>
+        </>
+    );
+};
+export default Login;
